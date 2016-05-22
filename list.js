77f3b49748dcea98e0dece4f4c76475975385926
MeMyselfandMilo <MeMyselfandMilo@users.noreply.github.com>
2016-05-22 22:10:02 +1000

    Update list.js

--- list.js
+++ list.js
@@ -1,3 +1,10 @@
+//To use list.js you must get the uid of the video that you want to add
+//eg: https://www.youtube.com/watch?v=OnNqxI5EdiI
+//                                 uid|_________|
+// You must then create a new part of the array
+//eg: list[0] = "uid"
+//This youtube video has now been added to the randomizer
 list[0] = "5NTqZ347TKY"
 list[1] = "JJxJPBKfeGk"
 list[2] = "ILnvtUirHr8"
+list[3] = "OnNqxI5EdiI"
